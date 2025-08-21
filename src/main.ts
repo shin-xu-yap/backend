f44536b51a43608cad995e72196e3f3f0179ea41
shin-xu-yap <shinxu.yap@gmail.com>
2025-08-21 13:21:23 +0800

    feat: Update CORS configuration to support dynamic frontend URL

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // your Next.js frontend
+    origin: [process.env.FRONTEND_URL || 'http://localhost:3000'],
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 4000);
+
+  const port = process.env.PORT || 4000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
