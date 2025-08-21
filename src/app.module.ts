4edb58cd11a078a3713e4c15f29677b9f451692d
shin-xu-yap <shinxu.yap@gmail.com>
2025-08-21 22:39:45 +0800

    fix: Refactor app module imports and update CORS settings to include localhost

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,18 @@
-import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
-// import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { PrismaModule } from './prisma/prisma.module';
-import { JobModule } from './job/job.module';
-import { FavoriteJobModule } from './favorite-job/favorite-job.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ElasticModule } from './elastic/elastic.module';
+import { JobResolver } from './job/job.resolver';
 
 @Module({
   imports: [
-    PrismaModule,
-    JobModule,
-    FavoriteJobModule,
-    GraphQLModule.forRoot({
+    ElasticModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       autoSchemaFile: true,
       playground: true,
-      cors: {
-        origin: [
-          'https://frontend-lilac-one-21.vercel.app',
-          'http://localhost:3000',
-        ],
-        credentials: true,
-      },
     }),
   ],
+  providers: [JobResolver],
 })
 export class AppModule {}
