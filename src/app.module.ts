75c23f5b4960a00e21709d8e2b470c964e93b50c
shin-xu-yap <shinxu.yap@gmail.com>
2025-08-22 00:12:12 +0800

    fix: add ElasticModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { PrismaModule } from './prisma/prisma.module';
 import { JobModule } from './job/job.module';
 import { FavoriteJobModule } from './favorite-job/favorite-job.module';
+import { ElasticModule } from './elastic/elastic.module';
 
 @Module({
   imports: [
+    ElasticModule,
     PrismaModule,
     JobModule,
     FavoriteJobModule,
